83ef03339d61df10e24307e261540d7daa351a7c
Alex Kazak <arkantos781227@mail.ru>
2020-10-06 11:04:39 +0200

    add title to html

---
+++ practice/Anaida_page.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="css/style.css">
+		<title>Anaida - beautiful violinist</title>
+	</head>
+	<body>
+		<div class="container">
+			<?php
+				include "header.php";
+			?>
+			<div class="content">
+				<div class="Hello_portfolio">
+					<div class="Hello_portfolio_3box">
+						<h1 class="main_title_portfolio">Isakhanova Anaida</h1>
+					<h3 class="first_description_portfolio">Beautiful and Tallented violinist.</h3>
+					</div>
+					<div class="picture1">
+						
+					</div>
+				</div>
+				<div class="statistic">
+					<ul class="list_stat">
+						<li class="list1">list_1</li>
+						<li class="list2">list_2</li>
+						<li class="list3">list_3</li>
+						<li class="list4">list_4</li>
+					</ul>
+				</div>
+				<div class="photo">
+					<img src="img/IMG_1232.JPG" alt="photo" width="400">
+				</div>
+				<div class="infoIn"> 
+					<div class="infoInIn">
+					<h2>Contact Info:</h2>
+						<p>To give give beginning divide, cattle. Give moving <br>
+							won't, there the abundantly she'd she'd brought air <br>
+							upon. Light hath subdue. Life days creature upon <br>
+						first heaven gathering dry.</p>
+						<p> won't, there the abundantly she'd she'd brought air <br>
+							upon. Light hath subdue. Life days creature upon <br>
+						first heaven gathering dry.</p>
+						<img src="" alt="signature">
+				</div>
+					<div class="picture2">
+						
+					</div>
+				</div>
+				<div class="needAproject">
+					<h2>Need a Project?</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br> Vitae laborum esse cum officia sunt perspiciatis.</p>
+					<input class="letsTalk" type="submit" value="LETS TALK">
+				</div>
+				
+			</div>
+			<div class="footer">
+				<ul class="list_Inc">
+					<li class="Inc"><p>Ad Libitum, Inc.</p></li>
+					<li><p>&copy; 2017 Kazak. All rights reserved.</p></li>
+					<li><p>Designed by "Not Me"</p></li>
+				</ul>
+				<ul class="list_contacts">
+					<li><p>arkantos781227@mail.ru</p></li>
+					<li><p>+375 29 328 6927</p></li>
+				</ul>
+				<ul class="info" id="social">
+					<li><p>Facebook</p></li>
+					<li><p>Instagram</p></li>
+					<li><p>VK</p></li>
+					<li><p>WhatsApp</p></li>
+				</ul>
+				
+				<ul class="info" id="news">
+					<li><p>News</p></li>
+					<li><p>Events</p></li>
+					<li><p>Contacts</p></li>
+					<li><p>Legals</p></li>
+				</ul>
+				<ul class="info" id="projects">
+					<li><p>Projects</p></li>
+					<li><p>About</p></li>
+					<li><p>Services</p></li>
+					<li><p>Carreer</p></li>
+				</ul>
+			</div>
+		</div>
+		
+		
+	</body>
+</html>
